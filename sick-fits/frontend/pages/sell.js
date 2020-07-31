d30cc4a6d12636d3798d6a16ce6d6073eb898113
Marius-De-Allie <spectre4300s@yahoo.com>
2020-07-31 11:48:54 -0400

    Create Sell component in 'src/pages/sell.js' file

---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// Next Link component, similar to react router Link component.
+import Link from 'next/link';
+
+const Sell = () => (
+    <div className="sell-page">
+        <h1>SellPage!</h1>
+    </div>
+);
+
+export default Sell;
