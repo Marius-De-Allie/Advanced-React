b84541ef187276fdf364e9b9ffa4338029d9733a
Marius-De-Allie <spectre4300s@yahoo.com>
2020-07-30 21:57:37 -0400

    Create new createItem Mutation in and items Query in the public facing API GQL schema, declare items Query resolver and createItem Mutation resolver

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,13 @@
-const Mutation = {};
+const Mutation = {
+    async createItem(parent, args, ctx, info) {
+        // TODO checkif they are logged in.
+        const item = await ctx.db.mutation.createItem({
+            data: {
+                ...args
+            }
+        }, info);
+        return item;
+    }
+};
 
 module.exports = Mutation;
