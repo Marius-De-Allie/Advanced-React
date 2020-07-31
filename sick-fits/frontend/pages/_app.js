e95c52bfe6acdbebef4a2cae1bad80741a05961a
Marius-De-Allie <spectre4300s@yahoo.com>
2020-07-31 12:31:13 -0400

    Declare getInitialProps nextjs lifecycle method within MyApp component

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -4,11 +4,22 @@ import { ApolloProvider } from 'react-apollo';
 import withData from '../lib/withData';
 // Custom Nextjs App component.
 class MyApp extends App {
+
+    static async getInitialProps({ Component, ctx}) {
+        let pageProps = {};
+        if(Component.getInitialProps) {
+            page.Props = await Component.getInitialProps(ctx);        
+        }
+        // Exposes the query to the user.
+        pageProps.query = ctx.query
+        return { pageProps };
+    }
     
     render() {
-        const { Component, apollo } = this.props;
+        const { Component, apollo, pageProps } = this.props;
         return (
             <Container>
+                {/*Wrap entire app in ApolloProvider, so that entire app has access to apollo data*/}
                 <ApolloProvider client={apollo}>
                     <Page>
                         <Component />
