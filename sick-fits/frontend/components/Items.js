62ee79f1e09e5510ff930dcf4a02c85ec263c8cf
Marius-De-Allie <spectre4300s@yahoo.com>
2020-08-01 01:14:17 -0400

    Declare ALL_ITEMS_QUERY function that queries graphQL items type within Items component

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+
+// GQL query to retrive all items from backend.
+const ALL_ITEMS_QUERY = gql`
+    query ALL_ITEMS_QUERY {
+        items {
+            id
+            title
+            price
+            description
+            image
+            largeImage
+        }
+    }
+`;
 
 class Items extends React.Component {
 
     render() {
-        <div>
-            Items
-        </div>
-
+        return (
+            <div>
+                <Query query={ALL_ITEMS_QUERY}>
+                    {({ data, error, loading }) => {
+                        if(loading) {
+                            <p>Loading...</p>
+                        } else if(error) {
+                            <p>Error: {error.message}</p>
+                        } else {
+                            console.log(data.items);
+                            return <p>{`Found ${data.items.length} item${data.items.length === 1 ? '' : 's'}`}</p>
+                        }
+                    }}
+                </Query>
+            </div>
+        );
     }
 };
 
