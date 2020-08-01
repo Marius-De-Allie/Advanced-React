2ce54796659a28666051ef0655f4a2fe51e3108a
Marius-De-Allie <spectre4300s@yahoo.com>
2020-08-01 00:37:45 -0400

    Create class based Items component

---
+++ sick-fits/frontend/components/Items.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Query } from 'react-apollo';
+
+class Items extends React.Component {
+
+    render() {
+        <div>
+            Items
+        </div>
+
+    }
+};
+
+export default Items;
