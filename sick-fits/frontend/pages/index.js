1188ee2811b9788b8d6d6922476d367d0e02de33
Marius-De-Allie <spectre4300s@yahoo.com>
2020-08-01 00:41:53 -0400

    Import and render Items component within Home component

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Link from 'next/link';
+// Import Items Comp.
+import Items from '../components/Items';
 
 const Home = () => (
     <div>
-        <h1>HomePage</h1>
+        <Items />
     </div>
 );
 
