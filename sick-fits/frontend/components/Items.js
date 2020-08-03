ae35f042d88f01588d2274538651a57ac1ced7d4
Marius-De-Allie <spectre4300s@yahoo.com>
2020-08-03 16:00:14 -0400

    Update ALL_ITEMS_QUERY gql query to take in first, skip and orderBy arguments, add variable prop to Query component within Items component render method

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -4,11 +4,12 @@ import gql from 'graphql-tag';
 import styled from 'styled-components';
 import Item from './Item';
 import Pagination from './Pagination';
+import { perPage } from '../config';
 
 // GQL query to retrive all items from backend.
 const ALL_ITEMS_QUERY = gql`
-    query ALL_ITEMS_QUERY {
-        items {
+    query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+        items(first: $first, skip: $skip, orderBy: createdAt-DESC) {
             id
             title
             price
@@ -47,7 +48,14 @@ class Items extends React.Component {
         return (
             <Center>
                 <Pagination page={this.props.page} />
-                <Query query={ALL_ITEMS_QUERY}>
+                <Query 
+                    query={ALL_ITEMS_QUERY}
+                    // fetchPolicy="network-only"
+                    variables={{
+                        skip: this.props.page * perPage - perPage,
+                        first: perPage
+                    }}
+                >
                     {({ data, error, loading }) => {
                         if(loading) {
                             <p>Loading...</p>
