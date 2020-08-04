0a34b7b5521b56b303ad44d6c17f1923d611b44f
Marius-De-Allie <spectre4300s@yahoo.com>
2020-08-04 14:11:51 -0400

    Have YOGA server use the jwt token

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,4 +1,5 @@
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 // import GQL-YOGA screateServer func.
 const createServer = require('./createServer');
@@ -11,6 +12,16 @@ const server = createServer();
 // TODO Use express middleware to handle cookies (JWT).
 server.express.use(cookieParser());
 // TODO Use express middleware to populate current user.
+// decode the JWT so we can get the user id on each request.
+server.express.use((req, res, next) => {
+    const { token} = req.cookies;
+    if(token) {
+        const { userId } = jwt.verify(token, process.env.APP_SECRET);
+        // Put userId onto the request for further request to access.
+        req.userId = userId;
+    }
+    next();
+});
 
 // Start up YOGA server.
 server.start({
