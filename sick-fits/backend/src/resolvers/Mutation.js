e2cee1f8428f67d2cea507b453bf328462e8c246
Marius-De-Allie <spectre4300s@yahoo.com>
2020-08-03 22:35:12 -0400

    Declare signup mutation resolver that creates JWT token and sets token as cookie on the context response

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,7 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { SingleFieldSubscriptions } = require("graphql/validation/rules/SingleFieldSubscriptions");
+
 const Mutation = {
     async createItem(parent, args, ctx, info) {
         // TODO checkif they are logged in.
@@ -31,6 +35,30 @@ const Mutation = {
 
         // Delete Item.
         return ctx.db.mutation.deleteItem({ where }, info);
+    },
+    async signup(parent, args, ctx, info) {
+        // Set email to lowercase string.
+        args.email = args.email.toLowerCase();
+        // Hash the password.
+        const password = await bcrypt.hash(args.password, 10);
+        // Create user in db.
+        const user = await ctx.db.mutation.createUser({
+            data: {
+                ...args,
+                password,
+                // set user's default permissions
+                permissions: { set: ['USER'] }
+            }
+        }, info);
+        // Create a JWT token for user.
+        const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+        // Set jwt as cookie on response.
+        ctx.response.cookie('token', token, {
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60 * 24 * 365 //1 year cookie
+        });
+        // we return the user to the browser.
+        return user;
     }
 };
 
