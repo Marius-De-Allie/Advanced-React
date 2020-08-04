cb2f74581372a660a5aae8b5a9faca7358e27a69
Marius-De-Allie <spectre4300s@yahoo.com>
2020-08-04 16:27:33 -0400

    Create functional Signout component

---
+++ sick-fits/frontend/components/Signout.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+// TODO add CURRENT_USER_MUTATION when it's created.
+
+const Signout = props => <button>SignOut</button>;
+
+export default Signout;
