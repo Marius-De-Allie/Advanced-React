edf9500a575db9bccb41778259bed6b49a51f936
Marius-De-Allie <spectre4300s@yahoo.com>
2020-08-04 16:47:21 -0400

    Declare SIGN_OUT_MUTATION gql mutation in Signout component and wrap component in Mutation component

--- sick-fits/frontend/components/Signout.js
+++ sick-fits/frontend/components/Signout.js
@@ -3,6 +3,21 @@ import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 // TODO add CURRENT_USER_MUTATION when it's created.
 
-const Signout = props => <button>SignOut</button>;
+const SIGN_OUT_MUTATION = gql`
+    mutation SIGN_OUT_MUTATION {
+        signout {
+            message
+        }
+    }
+`;
+
+const Signout = props => (
+    <Mutation 
+        mutation={SIGN_OUT_MUTATION}
+        refetchQueries={[{ query: CURRENT_USER_QUERY }]}
+    >
+        {(signout) => <button onClick={signout}>SignOut</button>}
+    </Mutation>
+);
 
 export default Signout;
