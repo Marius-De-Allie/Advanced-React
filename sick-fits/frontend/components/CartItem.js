d4cdf26e6a88b084b923481af6809cde1d34b59b
Marius-De-Allie <spectre4300s@yahoo.com>
2020-08-05 13:37:43 -0400

    Create functional CartItem component

---
+++ sick-fits/frontend/components/CartItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import formatMoney from '../lib/formatMoney';
+import styled from 'styled-components';
+
+const cartItemsStyles = styled.li`
+
+`;
+
+const CartItem = props => (
+    <cartItemsStyles>hi</cartItemsStyles>
+);
+
+export default CartItem;
