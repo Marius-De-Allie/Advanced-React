aa6121eaa8fb6a4e8a26772b73f116b30a25174e
Marius-De-Allie <spectre4300s@yahoo.com>
2020-08-06 10:55:07 -0400

    Declare createOrder Query resolver

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,4 +1,5 @@
 const { forwardTo } = require("prisma-binding");
+const { createOrder } = require("./Mutation");
 
 const Query = {
     async items(parent, args, ctx, info) {
@@ -9,7 +10,27 @@ const Query = {
         const item = await ctx.db.query.item();
         return item;
     },
-    itemsConnection: forwardTo('db')
+    itemsConnection: forwardTo('db'),
+
+
+
+    async createOrder(parent, args, ctx, info) {
+        // Make sure user is logged in.
+        if(!ctx.request.userId) {
+            throw new Error(`You are not logged in!`);
+        }
+        // Query current order.
+        const order = await ctx.db.query.order({ 
+            where: { id: args.id}, info});
+        // check if they have permissions to see order
+        const ownsOrder = order.user.id === ctx.request.userId;
+        const hasPermissionToSeeOrder = ctx.request.user.permissions.includes('ADMIN');
+        if(!ownsOrder || !hasPermissionToSeeOrder) {
+            throw new Error(`You can't see this order`);
+        }
+        // return order.
+        return order;
+    }
 };
 
 module.exports = Query;
