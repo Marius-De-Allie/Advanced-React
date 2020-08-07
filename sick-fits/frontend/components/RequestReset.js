9f672f3d5247aad2b53688aa37d428ca67d2d184
Marius-De-Allie <spectre4300s@yahoo.com>
2020-08-06 23:17:59 -0400

    Create RequestRest and ResetPage components

---
+++ sick-fits/frontend/components/RequestReset.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag';
+import Form from './styles/Form';
+import ErrorMessage from './ErrorMessage';
+
+const REQUEST_RESET_MUTATION = gql`
+    mutation SREQUEST_RESET_MUTATION($email: String!) {
+        requestReset(email: $email) {
+            message
+        }
+    }
+`;
+
+class RequestReset extends Component {
+    // Component state.
+    state = {
+        email: ''
+    }
+
+    handleChange = ({ target }) => {
+        this.setState((prevState) => ({
+            [target.name]: target.value
+        }));
+    }
+
+    onSubmit = async (evt, mutation) => {
+            evt.preventDefault();
+        try {
+            const response = await mutation();
+            // Clear input fields.
+            this.setState(() => ({
+                email: ''
+            }));
+        } catch(error) {
+            console.log(error.message)
+        }
+    };
+ 
+    render() {
+        const { email, name, password } = this.state;
+        return (
+            <Mutation
+                mutation={REQUEST_RESET_MUTATION}
+                variables={this.state}
+            >
+                {(reset, { error, loading, called }) => {
+                    return (
+                        <Form onSubmit={evt => {this.onSubmit(evt, reset)}} method="post">
+                            <fieldset disabled={loading} aria-busy={loading}>
+                                <h2>Request a password reset</h2>
+                                <ErrorMessage error={error} />
+                                {!error && !loading && called && <p>Success! Check your email for a reset link</p>}
+                                <label htmlFor="signin-email">Email</label>
+                                <input
+                                    id="signin-email"
+                                    type="email"
+                                    name="email"
+                                    value={email}
+                                    placeholder="Enter email"
+                                    onChange={this.handleChange}
+                                />
+                                <button type="submit">Request Reset</button>
+                            </fieldset>
+                        </Form>
+                    );
+                }}
+            </Mutation>
+        );
+    }
+};
+
+export default RequestReset;
---
+++ sick-fits/frontend/pages/reset.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// Next Link component, similar to react router Link component.
+import Link from 'next/link';
+
+const Reset = () => (
+    <div>
+        <p>reset your password</p>
+    </div>
+);
+
+export default Reset;
